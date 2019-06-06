bb254718b9229d4ed313b1b03b23215495086f25
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-06 14:41:50 -0300

    Criado: pacote login, classe java LoginActivity e XML activity_login

---
+++ app/src/main/java/com/system/operational/sgo/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.system.operational.sgo.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.system.operational.sgo.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
