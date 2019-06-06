59fec1914583b61b274b72cf9a9afa0b93250903
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-06 15:50:05 -0300

    Atribuido a classe LoginActivity: atributos do formulario xml login e suas funções respectivas

--- app/src/main/java/com/system/operational/sgo/login/LoginActivity.java
+++ app/src/main/java/com/system/operational/sgo/login/LoginActivity.java
@@ -1,15 +1,69 @@
 package com.system.operational.sgo.login;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+import android.widget.Button;
+
+import com.system.operational.sgo.MainActivity;
 import com.system.operational.sgo.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private AppCompatEditText editTextEmail;
+    private TextInputLayout textInputLayoutEmail;
+    private AppCompatEditText editTextSenha;
+    private TextInputLayout textInputLayoutSenha;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        castLayout();
+        buttonLogin();
+
+    }
+
+    public void castLayout() {
+        editTextEmail = findViewById(R.id.form_login_login_Id);
+        textInputLayoutEmail = findViewById(R.id.txt_layout_login_Id);
+        editTextSenha = findViewById(R.id.form_login_senha_Id);
+        textInputLayoutSenha = findViewById(R.id.txt_layout_senha_Id);
+        btnLogin = findViewById(R.id.btn_login_Id);
+    }
+
+    public void buttonLogin(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validateForm())
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private boolean validateForm() {
+        boolean formularioPreenchido = true;
+        if (editTextEmail.getText().toString().isEmpty()) {
+            textInputLayoutEmail.setErrorEnabled(true);
+            textInputLayoutEmail.setError("Informe seu email.");
+            formularioPreenchido = false;
+        } else {
+            textInputLayoutEmail.setErrorEnabled(false);
+        }
+        if (editTextSenha.getText().toString().isEmpty()) {
+            textInputLayoutSenha.setErrorEnabled(true);
+            textInputLayoutSenha.setError("Informe sua senha.");
+            formularioPreenchido = false;
+        } else {
+            textInputLayoutSenha.setErrorEnabled(false);
+        }
+        return formularioPreenchido;
     }
 }
