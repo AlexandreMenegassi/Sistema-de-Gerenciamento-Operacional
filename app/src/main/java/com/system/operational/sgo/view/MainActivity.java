ecc572a1df8959754656516f0628794827bbae6f
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-12 21:42:03 -0300

    Criado referências do xml e implementado sua estrutura para nevegação das telas.

--- app/src/main/java/com/system/operational/sgo/view/MainActivity.java
+++ app/src/main/java/com/system/operational/sgo/view/MainActivity.java
@@ -1,15 +1,55 @@
 package com.system.operational.sgo.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.system.operational.sgo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button atualizarEstacao;
+    Button cadastrarPendencias;
+    Button vizualizarEstacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        castLayout();
+        buttons();
+
+    }
+
+    public void castLayout(){
+        atualizarEstacao = findViewById(R.id.atualizarEstacaoId);
+        cadastrarPendencias = findViewById(R.id.cadastrarPendenciasId);
+        vizualizarEstacao = findViewById(R.id.vizualizarEstacaoId);
+    }
+
+    public void buttons(){
+        atualizarEstacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AtualizarEstacaoActivity.class));
+            }
+        });
+
+        cadastrarPendencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CadastrarPendenciasActivity.class));
+            }
+        });
+
+        vizualizarEstacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, VisualizarEstacaoActivity.class));
+            }
+        });
     }
 }
