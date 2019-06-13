baecb08628c2242046da47fdb8fab49f96366e21
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-13 19:57:05 -0300

    Adicionado função listar estações na classe java Controller

--- app/src/main/java/com/system/operational/sgo/controller/Controller.java
+++ app/src/main/java/com/system/operational/sgo/controller/Controller.java
@@ -1,4 +1,49 @@
 package com.system.operational.sgo.controller;
 
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.system.operational.sgo.DAO.Database;
+import com.system.operational.sgo.controller.list.atualizarEstacao.AtualizarEstacaoListAdapter;
+import com.system.operational.sgo.controller.list.atualizarEstacao.OnListClickInteractionListenerAtualizarEstacao;
+import com.system.operational.sgo.model.Estacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    public static ViewHolder mViewHolderAtualizarEstacao = new ViewHolder();
+    public static Estacao estacao;
+
+    //LIST
+    public static List<Estacao> listAtualizarEstacao = new ArrayList<>();
+
+    public static void listarCliente(Context context, RecyclerView recyclerView) {
+
+        Database.lerEstacoes();
+
+        try {
+            OnListClickInteractionListenerAtualizarEstacao listener = new OnListClickInteractionListenerAtualizarEstacao() {
+                @Override
+                public void onClick(int id) {
+
+                }
+            };
+            mViewHolderAtualizarEstacao.recyclerViewAtualizarEstacao = recyclerView;
+
+            AtualizarEstacaoListAdapter atualizarEstacaoListAdapter = new AtualizarEstacaoListAdapter(listAtualizarEstacao, listener);
+            mViewHolderAtualizarEstacao.recyclerViewAtualizarEstacao.setAdapter(atualizarEstacaoListAdapter);
+
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
+            mViewHolderAtualizarEstacao.recyclerViewAtualizarEstacao.setLayoutManager(linearLayoutManager);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static class ViewHolder {
+        RecyclerView recyclerViewAtualizarEstacao;
+    }
 }
