3a0d9a0347c4dae849bf095882af125496a8f2ca
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-13 01:18:29 -0300

    Atribuido a classe URL seus paramentros e caminho do arquivo que fara a conexão ao banco de dados.

--- app/src/main/java/com/system/operational/sgo/DAO/URL.java
+++ app/src/main/java/com/system/operational/sgo/DAO/URL.java
@@ -2,4 +2,12 @@ package com.system.operational.sgo.DAO;
 
 public class URL {
 
+    private static final String ROOT_URL = "http://192.168.0.188/opt/lampp/htdocs/ezentis/application/config/Api.php?apicall=";
+
+    public static final String URL_ATUALIZAR_ESTACAO = ROOT_URL + "createhero";
+    public static final String URL_LISTAR_ESTACAO = ROOT_URL + "getheroes";
+
+    public static final String URL_CADASTRAR_PENDENCIAS = ROOT_URL + "updatehero";
+    public static final String URL_LISTAR_PENDENCIA = ROOT_URL + "deletehero&id=";
+
 }
