3df4cc98f7814ac397b421fb40fea3f477ea91f3
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-13 19:59:50 -0300

    Atribuido estrutura da classe AtualizarEstacaoActivity para listagem do recyclerView

--- app/src/main/java/com/system/operational/sgo/view/AtualizarEstacaoActivity.java
+++ app/src/main/java/com/system/operational/sgo/view/AtualizarEstacaoActivity.java
@@ -2,17 +2,23 @@ package com.system.operational.sgo.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
+import com.system.operational.sgo.DAO.Database;
 import com.system.operational.sgo.R;
+import com.system.operational.sgo.controller.Controller;
 
 public class AtualizarEstacaoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atualizar_estacao);
 
-
+        recyclerView = findViewById(R.id.recyclerViewAtualizarEstacaoId);
+        Controller.listarCliente(this, recyclerView);
 
     }
 }
