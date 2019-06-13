e4854ec7d17135e37208a2d7a192144a1218fa14
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-12 21:34:20 -0300

    Atribuido ao pacote view e layout, classe java e xml das telas atualizar estação, cadastrar pendencias e visualizar estacao.

---
+++ app/src/main/java/com/system/operational/sgo/view/VisualizarEstacaoActivity.java
@@ -0,0 +1,15 @@
+package com.system.operational.sgo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.system.operational.sgo.R;
+
+public class VisualizarEstacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_estacao);
+    }
+}
