5002bf76c26af203238cd403e12470613eb9c757
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-15 03:21:47 -0300

    Adicionando ao pacote model: Usuario.java classe de modelo para autenticacao do usuario

---
+++ app/src/main/java/com/system/operational/sgo/model/Usuario.java
@@ -0,0 +1,32 @@
+package com.system.operational.sgo.model;
+
+public class Usuario {
+
+    public int id_usuario;
+    public String nome;
+    public int nivel;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+}
