e0a3ed8f9722480982f4eaf9d805f52d8c709512
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-18 02:07:03 -0300

    Refatorando codigo e corrigindo caminhos da URL

--- app/src/main/java/com/system/operational/sgo/DAO/URL.java
+++ app/src/main/java/com/system/operational/sgo/DAO/URL.java
@@ -3,16 +3,21 @@ package com.system.operational.sgo.DAO;
 public class URL {
 
     //ACESSO VIA LOCALHOST
-    private static final String ROOT_URL = "http://192.168.0.109/ezentisApp/database/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.0.102/ezentisApp/database/Api.php?apicall=";
 
     //ACESSO VIA FACULDADE
     //private static final String ROOT_URL = "http://192.168.0.188/ezentisApp/database/Api.php?apicall=";
 
-    public static final String URL_LISTAR_ESTACAO = ROOT_URL + "atualizarEstacao";
+    public static final String URL_LISTAR_ESTACAO = ROOT_URL + "listarEstacao";
 
     public static final String URL_VERIFICAR_USUARIO = ROOT_URL + "verificarUsuario&login=";
 
-    //public static final String URL_AUTENTICAR_USUARIO = ROOT_URL + "";
+    public static final String URL_ATUALIZAR_ESTACAO = ROOT_URL + "atualizarEstacao&acesso=";
+
 
     //public static final String URL_CADASTRAR_PENDENCIAS = ROOT_URL + "";
+
+    public static final int CODE_GET_REQUEST = 1024;
+
+    public static final int CODE_POST_REQUEST = 1025;
 }
