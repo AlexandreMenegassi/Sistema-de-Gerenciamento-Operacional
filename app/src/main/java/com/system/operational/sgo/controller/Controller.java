8b5c58269c529dd84e9be7e9736f5d78cdbf41ae
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-18 02:00:50 -0300

    Refatorando codigo e adicionando funcionalidades para trocar de activity

--- app/src/main/java/com/system/operational/sgo/controller/Controller.java
+++ app/src/main/java/com/system/operational/sgo/controller/Controller.java
@@ -1,47 +1,63 @@
 package com.system.operational.sgo.controller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.system.operational.sgo.DAO.Database;
-import com.system.operational.sgo.controller.list.atualizarEstacao.AtualizarEstacaoListAdapter;
-import com.system.operational.sgo.controller.list.atualizarEstacao.OnListClickInteractionListenerAtualizarEstacao;
+import com.system.operational.sgo.DAO.DatabaseList;
+import com.system.operational.sgo.controller.list.listaDeEstacao.ListaDeEstacaoListAdapter;
+import com.system.operational.sgo.controller.list.listaDeEstacao.OnListClickInteractionListenerListaDeEstacao;
 import com.system.operational.sgo.model.Estacao;
+import com.system.operational.sgo.model.Usuario;
+import com.system.operational.sgo.view.forms.FormularioAtualizarEstacaoActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
 
-    public static ViewHolder mViewHolderAtualizarEstacao = new ViewHolder();
+    public static ViewHolder mViewHolderListaDeEstacao = new ViewHolder();
+    public static List<Estacao> listaDeEstacao = new ArrayList<>();
+    public static Usuario USUARIO = new Usuario();
+    public static List ESTACAO = new ArrayList();
+    public static String message;
 
-    public static List<Estacao> listAtualizarEstacao = new ArrayList<>();
+    public static void listaDeEstacao(final Context context, RecyclerView recyclerView, final int op) {
 
-    public static void listarCliente(Context context, RecyclerView recyclerView) {
-
-        Database.lerEstacoes();
+        DatabaseList.lerEstacoes();
 
         try {
-            OnListClickInteractionListenerAtualizarEstacao listener = new OnListClickInteractionListenerAtualizarEstacao() {
+            OnListClickInteractionListenerListaDeEstacao listener = new OnListClickInteractionListenerListaDeEstacao() {
                 @Override
                 public void onClick(int id) {
+                    switch (op) {
+                        case 0:
+                            context.startActivity(new Intent(context, FormularioAtualizarEstacaoActivity.class));
+                            break;
+                        case 1:
 
+                            break;
+                        case 2:
+
+                            break;
+                    }
                 }
             };
-            mViewHolderAtualizarEstacao.recyclerViewAtualizarEstacao = recyclerView;
+            mViewHolderListaDeEstacao.recyclerViewListaDeEstacao = recyclerView;
 
-            AtualizarEstacaoListAdapter atualizarEstacaoListAdapter = new AtualizarEstacaoListAdapter(listAtualizarEstacao, listener);
-            mViewHolderAtualizarEstacao.recyclerViewAtualizarEstacao.setAdapter(atualizarEstacaoListAdapter);
+            ListaDeEstacaoListAdapter listaDeEstacaoListAdapter = new ListaDeEstacaoListAdapter(listaDeEstacao, listener);
+            mViewHolderListaDeEstacao.recyclerViewListaDeEstacao.setAdapter(listaDeEstacaoListAdapter);
 
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
-            mViewHolderAtualizarEstacao.recyclerViewAtualizarEstacao.setLayoutManager(linearLayoutManager);
+            mViewHolderListaDeEstacao.recyclerViewListaDeEstacao.setLayoutManager(linearLayoutManager);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     public static class ViewHolder {
-        RecyclerView recyclerViewAtualizarEstacao;
+        RecyclerView recyclerViewListaDeEstacao;
     }
 }
