adbc7a92a026ec1f5a2bc8746e601360d01e0797
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-18 02:04:29 -0300

    Refatorando codigo e removendo mensagem toast

--- app/src/main/java/com/system/operational/sgo/DAO/DatabaseList.java
+++ app/src/main/java/com/system/operational/sgo/DAO/DatabaseList.java
@@ -13,11 +13,8 @@ import java.util.HashMap;
 
 public class DatabaseList {
 
-    private static final int CODE_GET_REQUEST = 1024;
-    private static final int CODE_POST_REQUEST = 1025;
-
     public static void lerEstacoes() {
-        PerformNetworkRequest request = new PerformNetworkRequest(URL.URL_LISTAR_ESTACAO, null, CODE_POST_REQUEST);
+        PerformNetworkRequest request = new PerformNetworkRequest(URL.URL_LISTAR_ESTACAO, null, URL.CODE_POST_REQUEST);
         request.execute();
     }
 
@@ -37,7 +34,6 @@ public class DatabaseList {
             try {
                 JSONObject object = new JSONObject(s);
                 if (!object.getBoolean("error")) {
-                    Controller.message = object.getString("message");
                     listarEstacoes(object.getJSONArray("lista"));
                 }
             } catch (JSONException e) {
@@ -47,23 +43,22 @@ public class DatabaseList {
 
         protected String doInBackground(Void... voids) {
             RequestHandler requestHandler = new RequestHandler();
-            if (requestCode == CODE_POST_REQUEST) return requestHandler.sendPostRequest(url, params);
-            if (requestCode == CODE_GET_REQUEST) return requestHandler.sendGetRequest(url);
+            if (requestCode == URL.CODE_POST_REQUEST) return requestHandler.sendPostRequest(url, params);
+            if (requestCode == URL.CODE_GET_REQUEST) return requestHandler.sendGetRequest(url);
             return null;
         }
     }
 
     public static void listarEstacoes(@org.jetbrains.annotations.NotNull JSONArray station) throws JSONException {
-        Controller.listAtualizarEstacao.clear();
+        Controller.listaDeEstacao.clear();
         for (int i = 0; i < station.length(); i++) {
-            JSONObject obj = station.getJSONObject(i);
-            Controller.listAtualizarEstacao.add(new Estacao(
-                obj.getInt("idestacao"),
-                obj.getString("endid"),
-                obj.getString("nomeestacao"),
-                obj.getString("regional"),
-                obj.getString("uf"),
-                obj.getString("cidade")));
+            Controller.listaDeEstacao.add(new Estacao(
+                station.getJSONObject(i).getInt("idestacao"),
+                station.getJSONObject(i).getString("endid"),
+                station.getJSONObject(i).getString("nomeestacao"),
+                station.getJSONObject(i).getString("regional"),
+                station.getJSONObject(i).getString("uf"),
+                station.getJSONObject(i).getString("cidade")));
         }
     }
 }
