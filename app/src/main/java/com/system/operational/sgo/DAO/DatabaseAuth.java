80bb48ff2b7819300a735f593049c1b17380ee44
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-18 02:02:44 -0300

    Refatorando codigo, corrigindo bugs e melhorando o desempenho do carregamento dos dados

--- app/src/main/java/com/system/operational/sgo/DAO/DatabaseAuth.java
+++ app/src/main/java/com/system/operational/sgo/DAO/DatabaseAuth.java
@@ -1,8 +1,10 @@
 package com.system.operational.sgo.DAO;
 
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 import com.system.operational.sgo.controller.Controller;
+import com.system.operational.sgo.view.login.LoginActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,12 +14,8 @@ import java.util.HashMap;
 
 public class DatabaseAuth {
 
-    private static final int CODE_GET_REQUEST = 1024;
-    private static final int CODE_POST_REQUEST = 1025;
-    public static boolean USUARIO_AUTENTICADO = false;
-
     public static void verificarUsuario(String nome, String senha) {
-        PerformNetworkRequest request = new PerformNetworkRequest(URL.URL_VERIFICAR_USUARIO + nome + "&senha=" + senha, null, CODE_GET_REQUEST);
+        PerformNetworkRequest request = new PerformNetworkRequest(URL.URL_VERIFICAR_USUARIO + nome + "&senha=" + senha, null, URL.CODE_GET_REQUEST);
         request.execute();
     }
 
@@ -39,8 +37,11 @@ public class DatabaseAuth {
                 if (object.getBoolean("error")) {
                     Controller.message = object.getString("message");
                     autenticarUsuario(object.getJSONArray("dados"));
+                    Toast.makeText(LoginActivity.context, Controller.message, Toast.LENGTH_SHORT).show();
+                    LoginActivity.activity(LoginActivity.context);
                 } else {
                     Controller.message = object.getString("message");
+                    Toast.makeText(LoginActivity.context, Controller.message, Toast.LENGTH_SHORT).show();
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -49,18 +50,15 @@ public class DatabaseAuth {
 
         protected String doInBackground(Void... voids) {
             RequestHandler requestHandler = new RequestHandler();
-            if (requestCode == CODE_POST_REQUEST)
-                return requestHandler.sendPostRequest(url, params);
-            if (requestCode == CODE_GET_REQUEST) return requestHandler.sendGetRequest(url);
+            if (requestCode == URL.CODE_POST_REQUEST) return requestHandler.sendPostRequest(url, params);
+            if (requestCode == URL.CODE_GET_REQUEST) return requestHandler.sendGetRequest(url);
             return null;
         }
     }
 
     public static void autenticarUsuario(@org.jetbrains.annotations.NotNull JSONArray station) throws JSONException {
-        JSONObject obj = station.getJSONObject(0);
-        Controller.USUARIO.setId_usuario(obj.getInt("idusuario"));
-        Controller.USUARIO.setNome(obj.getString("nome"));
-        Controller.USUARIO.setNivel(obj.getInt("nivel"));
-        if (!obj.getString("nome").isEmpty()) USUARIO_AUTENTICADO = true;
+        Controller.USUARIO.setId_usuario(station.getJSONObject(0).getInt("idusuario"));
+        Controller.USUARIO.setNome(station.getJSONObject(0).getString("nome"));
+        Controller.USUARIO.setNivel(station.getJSONObject(0).getInt("nivel"));
     }
 }
