39611fc8c7bcffb7b2fcd047e6a39121a53131bc
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-06-28 12:43:47 -0300

    Corrigindo valores do metodo e adicionando funcionalidade para startar activity

--- app/src/main/java/com/system/operational/sgo/view/list/VisualizarEstacaoActivity.java
+++ app/src/main/java/com/system/operational/sgo/view/list/VisualizarEstacaoActivity.java
@@ -1,12 +1,14 @@
 package com.system.operational.sgo.view.list;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
 import com.system.operational.sgo.R;
 import com.system.operational.sgo.controller.Controller;
+import com.system.operational.sgo.view.data.DadosEstacao;
 
 public class VisualizarEstacaoActivity extends AppCompatActivity {
 
@@ -20,6 +22,11 @@ public class VisualizarEstacaoActivity extends AppCompatActivity {
 
         context = this;
         recyclerView = findViewById(R.id.recyclerViewVisualizarEstacaoId);
-        Controller.listaDeEstacao(this, recyclerView, 2);
+        Controller.listaDeEstacao(this, recyclerView, 1);
+    }
+
+    public static void activity(Context contexto) {
+        Intent login = new Intent(contexto, DadosEstacao.class);
+        contexto.startActivity(login);
     }
 }
