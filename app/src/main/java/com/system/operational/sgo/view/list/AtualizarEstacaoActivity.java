5e6d68a7af879712bb649662b328b673f1a5cb65
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-07-12 23:54:47 -0300

    Adicionando função para filtragem de estação e refatorando nomes de métodos e caminho de Intents

--- app/src/main/java/com/system/operational/sgo/view/list/AtualizarEstacaoActivity.java
+++ app/src/main/java/com/system/operational/sgo/view/list/AtualizarEstacaoActivity.java
@@ -2,8 +2,13 @@ package com.system.operational.sgo.view.list;
 
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import com.system.operational.sgo.R;
 import com.system.operational.sgo.controller.Controller;
@@ -11,15 +16,28 @@ import com.system.operational.sgo.controller.Controller;
 public class AtualizarEstacaoActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private TextView procurar;
     public static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_atualizar_estacao);
+        setContentView(R.layout.activity_list_atualizar);
 
         context = this;
         recyclerView = findViewById(R.id.recyclerViewAtualizarEstacaoId);
+        procurar = findViewById(R.id.procurarEstacaoId);
         Controller.listaDeEstacao(this, recyclerView,0);
+
+        final AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextViewId);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Controller.LISTANOMEESTACAO);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+        procurar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Controller.filtarEstacao(AtualizarEstacaoActivity.this, recyclerView, autoCompleteTextView.getText().toString(), 0);
+            }
+        });
     }
 }
--- app/src/main/java/com/system/operational/sgo/view/list/VisualizarEstacaoActivity.java
+++ app/src/main/java/com/system/operational/sgo/view/list/VisualizarEstacaoActivity.java
@@ -5,29 +5,46 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import com.system.operational.sgo.R;
 import com.system.operational.sgo.controller.Controller;
 import com.system.operational.sgo.view.data.DadosEstacao;
-import com.system.operational.sgo.view.forms.FormularioAtualizarEstacaoActivity;
+import com.system.operational.sgo.view.form.FormularioInserirDadosEstacaoActivity;
 
 public class VisualizarEstacaoActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     public static Context context;
+    private TextView procurar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_visualizar_estacao);
+        setContentView(R.layout.activity_list_dados);
 
         context = this;
         recyclerView = findViewById(R.id.recyclerViewVisualizarEstacaoId);
+        procurar = findViewById(R.id.procurarDadosEstacaoId);
         Controller.listaDeEstacao(this, recyclerView, 1);
+
+        final AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteDadosTextViewId);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Controller.LISTANOMEESTACAO);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+        procurar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Controller.filtarEstacao(VisualizarEstacaoActivity.this, recyclerView, autoCompleteTextView.getText().toString(), 1);
+            }
+        });
     }
 
     public static void activityForm(Context contexto) {
-        Intent intent = new Intent(contexto, FormularioAtualizarEstacaoActivity.class);
+        Intent intent = new Intent(contexto, FormularioInserirDadosEstacaoActivity.class);
         contexto.startActivity(intent);
     }
 
