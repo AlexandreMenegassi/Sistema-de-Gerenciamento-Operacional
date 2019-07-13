ce65bb8f89c5422d1fdf3ac41bc0aacdd6c431ef
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-07-12 23:48:11 -0300

    Adicionando novo caminho de URL para inserção de uma nova atualização e corrigindo caminho para atualização da estação

--- app/src/main/java/com/system/operational/sgo/DAO/URL.java
+++ app/src/main/java/com/system/operational/sgo/DAO/URL.java
@@ -5,14 +5,16 @@ public class URL {
     public static final int CODE_GET_REQUEST = 1024;
     public static final int CODE_POST_REQUEST = 1025;
 
-    private static final String ROOT_URL = "http://192.168.15.3/ezentisApp/database/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.15.11/ezentisApp/database/Api.php?apicall=";
     //private static final String ROOT_URL = "http://192.168.0.188/ezentisApp/database/Api.php?apicall=";
 
     public static final String URL_LISTAR_ESTACAO = ROOT_URL + "listarEstacao";
 
     public static final String URL_VERIFICAR_USUARIO = ROOT_URL + "verificarUsuario&login=";
 
-    public static final String URL_ATUALIZAR_ESTACAO = ROOT_URL + "atualizarEstacao&acesso=";
+    public static final String URL_ADICIONAR_DADOS_ESTACAO = ROOT_URL + "adicionarDadosEstacao&acesso=";
+
+    public static final String URL_ATUALIZAR_DADOS_ESTACAO = ROOT_URL + "atualizarDadosEstacao&acesso=";
 
     public static final String URL_DADOS_ESTACAO = ROOT_URL + "dadosEstacao&id=";
 }
