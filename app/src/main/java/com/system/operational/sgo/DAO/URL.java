bdd47e6caa6f22b1ad8a4e01093f0f83712a94ae
Alexandre Sales <alexandre.menegassi@hotmail.com>
2019-07-16 02:31:05 -0300

    Adicionando nova URL para inserir nova pendencia

--- app/src/main/java/com/system/operational/sgo/DAO/URL.java
+++ app/src/main/java/com/system/operational/sgo/DAO/URL.java
@@ -5,7 +5,7 @@ public class URL {
     public static final int CODE_GET_REQUEST = 1024;
     public static final int CODE_POST_REQUEST = 1025;
 
-    private static final String ROOT_URL = "http://192.168.15.11/ezentisApp/database/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.15.4/ezentisApp/database/Api.php?apicall=";
     //private static final String ROOT_URL = "http://192.168.0.188/ezentisApp/database/Api.php?apicall=";
 
     public static final String URL_LISTAR_ESTACAO = ROOT_URL + "listarEstacao";
@@ -15,6 +15,8 @@ public class URL {
     public static final String URL_ADICIONAR_DADOS_ESTACAO = ROOT_URL + "adicionarDadosEstacao&acesso=";
 
     public static final String URL_ATUALIZAR_DADOS_ESTACAO = ROOT_URL + "atualizarDadosEstacao&acesso=";
+
+    public static final String URL_CADASTRAR_PENDENCIA = ROOT_URL + "cadastrarPendencia&pendencia=";
 
     public static final String URL_DADOS_ESTACAO = ROOT_URL + "dadosEstacao&id=";
 }
